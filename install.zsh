5b7aac23d6cf403a363cca7d40d9f856f221cac1
Mike Herrera <michael.herrera@gmail.com>
2015-10-18 17:46:47 -0700

    Changed to ZSH and fine tuned the output

--- install.zsh
+++ install.zsh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 #
 # author: Mike Herrera
 # source: github.com/crftr/dotfiles/install.sh
@@ -11,10 +11,10 @@ rm -f ~/.vimrc
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 
 echo "VIM: downloading vim-plug ..."
-curl -fLos ~/.vim/autoload/plug.vim --create-dirs \
+curl -s -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "VIM: downloading and installing plugins ..."
 vim +PlugInstall +qall
 
-echo "\nFIN!"
+echo "FIN!"
