883b2f710f67cc575bfeb187293e88a035e38bda
Mike Herrera <michael.herrera@gmail.com>
2015-10-18 17:42:54 -0700

    silence curl

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ rm -f ~/.vimrc
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
 
 echo "VIM: downloading vim-plug ..."
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLos ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "VIM: downloading and installing plugins ..."
