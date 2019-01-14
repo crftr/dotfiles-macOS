44eadb5eb72e47816be97713e5800ef337087d62
Mike Herrera <michael.herrera@gmail.com>
2019-01-13 19:52:26 -0800

    Spacemacs: Resolved startup errors & additional gui config

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -129,17 +129,20 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(gruvbox
-                         material
-                         afternoon
-                         darktooth
-                         sanityinc-tomorrow-eighties
-                         smyx
-                         tsdh-dark
-                         underwater
-                         zenburn
-                         spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes
+   '(
+     gruvbox
+     ;; material
+     ;; afternoon
+     ;; darktooth
+     ;; sanityinc-tomorrow-eighties
+     ;; smyx
+     ;; tsdh-dark
+     ;; underwater
+     ;; zenburn
+     spacemacs-dark
+     spacemacs-light
+     )
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
@@ -314,6 +317,8 @@ before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
 
   (setq-default git-magit-status-fullscreen t)
+  (load-file "~/.emacs.d/elpa/dash-20180910.1856/dash.el")
+  (load-file "~/.emacs.d/elpa/autothemer-20180920.923/autothemer.el")
   )
 
 (defun dotspacemacs/user-config ()
@@ -324,6 +329,10 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  (setq powerline-default-separator 'butt)
+  (setq neo-theme 'classic)
+
+  "Web-mode config"
   (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
   (setq web-mode-engines-alist
         '(("mustache" . "\\.tpl\\'"))
